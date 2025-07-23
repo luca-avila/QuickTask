7cefcd130e190721bb5645f66eef4d24ec94b86c
luca-avila <avilaluca61@gmail.com>
2025-07-23 18:58:41 -0300

    Implement run.py and POST route

---
+++ backend/app/routes/tasks.py
@@ -0,0 +1,57 @@
+from datetime import date
+from sqlalchemy import desc
+from flask import Blueprint, request, jsonify
+from backend.app.models.tasks import tasks, engine
+
+tasks_bp = Blueprint('tasks', __name__)
+
+def build_task_response(task):
+    return {
+        'id': task['id'],
+        'title': task['title'],
+        'description': task['description'] if task['description'] else None,
+        'priority': task['priority'],
+        'completed': task['completed'],
+        'date': task['date'].isoformat() if task['date'] else None
+    }
+
+@tasks_bp.route('/tasks', methods=['POST'])
+def add_task():
+    # Get json
+    data = request.json
+
+    # Return error if no JSON data received
+    if data is None:
+        return jsonify({'error': 'No JSON data received'}), 400
+
+    # Return error if not all required field are received
+    required_fields = ['title', 'completed', 'priority']
+    for field in required_fields:
+        if field not in data:
+            return jsonify({'error': f'Missing field: {field}'}), 400
+
+    # Return error if priority is not a number 0-5
+    try:
+        task_priority = int(data['priority'])
+        if task_priority < 0 or task_priority > 5:
+            return jsonify({'error': 'Priority must be between 0 and 5'}), 400
+    except (ValueError, TypeError):
+        return jsonify({'error': 'Priority should be a number'}), 400
+
+    # Return error if completed is not a boolean
+    if data['completed'] not in [True, False]:
+        return jsonify({'error': 'Completed should be a boolean'}), 400
+
+    # Add to the database
+    with engine.begin() as conn:
+        insert_statement = tasks.insert().values(
+            title = data['title'],
+            priority = task_priority,
+            completed = data['completed'],
+            description = data.get('description', ''),
+            date = date.today()
+        )
+        conn.execute(insert_statement)
+    
+    # Return success message
+    return jsonify({'message': 'Task succesfully added'}), 201
---
+++ run.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_cors import CORS
+from backend.app.routes.tasks import tasks_bp
+from backend.app.config import Config
+from backend.app.models.tasks import init_db
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    CORS(app)
+
+    app.register_blueprint(tasks_bp)
+    
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    init_db()
+    app.run(debug=True)
