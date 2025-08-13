2493e0b0f1639464edb420491283cc01a8a8460f
luca-avila <avilaluca61@gmail.com>
2025-08-13 14:43:43 -0300

    Implement testing of validation functions

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,4 +1,4 @@
-from backend.app.validators.task_validator import validate_task
+from backend.app.validators.task_validator import validate_task, validate_task_update
 
 def test_valid_task():
     valid_task = {
@@ -57,3 +57,12 @@ def test_invalid_type():
 def test_empty_task():
     invalid_task = {}
     assert validate_task(invalid_task) == False
+
+def test_valid_task_update():
+    valid_task = {
+        'title': 'Test Task',
+        'description': 'This is a test task',
+        'priority': 2,
+        'completed': False,
+    }
+    assert validate_task_update(valid_task) == ([], valid_task)
